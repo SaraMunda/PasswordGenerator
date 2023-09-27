63cf29adeab40fd4bbd676d3ac4251ae456b5346
Sara <saramunda99@gmail.com>
2023-09-27 15:52:52 +0200

    I added a button that creates a combined password with special characters and a button to copy both password

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 let password= ""
 let charset = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","U","V","W","X","Y","Z","0","1","2","3","4","5","6","7","8","9"]
-let generatedPass = document.getElementById("generatedPass")
+let specialCharacters = ["?","!","&","€","#","%","@","/","^","£"]
+let generatePass = document.getElementById("generatePass")
 let generateBtn = document.getElementById("generateBtn")
+let specialBtn = document.getElementById("specialBtn")
 
 function generatedRandomPassword(){
     password = "";
@@ -11,6 +13,44 @@ function generatedRandomPassword(){
         let randomCharset = Math.floor(Math.random() *charset.length)
         password += charset [randomCharset]
     }
-        generatePass.textContent = password
+        generatePass.value = password
 }
-generateBtn.addEventListener("click", generatedRandomPassword);
+generateBtn.addEventListener("click", generatedRandomPassword)
+
+
+
+function generatedSpecialPassword() {
+    password = "";
+    generatePass.value = "";
+
+    for (let i = 0; i < 6; i++) {
+        let randomSpecialCharacters = Math.floor(Math.random() * specialCharacters.length);
+        password += specialCharacters[randomSpecialCharacters];
+    }
+
+    generatePass.value = password ;
+}
+
+function generatedCombinedPassword() {
+    password = "";
+    generatePass.value = "";
+
+    let combinedCharset = charset.concat(specialCharacters);
+
+    for (let i = 0; i < 12; i++) {
+        let randomIndex= Math.floor(Math.random() * combinedCharset.length);
+        password += combinedCharset[randomIndex];
+    }
+
+    generatePass.value = password;
+}
+
+specialBtn.addEventListener("click", generatedCombinedPassword)
+
+
+function copyPassword() {
+    let copy = document.getElementById("generatePass")
+    copy.select()
+    navigator.clipboard.writeText(copy.value)
+    alert("Copied the text: " + copy.value)
+  }
